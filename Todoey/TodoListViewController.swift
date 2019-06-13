b8f988210c7df5c4b5a812f6b51248887edd6aad
Mateusz Ziobrowski <matrix.ziobrowski@gmail.com>
2019-06-13 08:53:43 +0100

    New table view

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Mateusz Ziobrowski on 13/06/2019.
+//  Copyright © 2019 Mateusz Ziobrowski. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let items: [String] = ["Work", "Shoppig", "Friends"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(items[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
